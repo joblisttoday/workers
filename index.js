dc660de6da0bdb201e096bf22682cc7e0dc6aa1b
hugurp <hugurp@pm.me>
2020-09-10 11:16:31 +0200

    add dotenv (for algolia keys), setup publish to algolia

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const config = require('dotenv').config()
+const algoliasearch = require("algoliasearch")
 const recruitee = require('./providers/recruitee')
 const greenhouse = require('./providers/greenhouse')
 const smartrecruiters = require('./providers/smartrecruiters')
 
-const city = 'berlin'
 
+/*
+	 test algolia
+ */
+
+const {
+	'ALGOLIA_DEV_APPLICATION_ID': algoliaAppId,
+	'ALGOLIA_DEV_ADMIN_API_KEY': algoliaApiKey
+} = config.parsed
+const indexName = 'dev_jobs'
+
+
+const client = algoliasearch(algoliaAppId, algoliaApiKey)
+const index = client.initIndex(indexName)
+
+
+/*
+	 test the providers
+ */
+
+const city = 'berlin'
 const recruiteeHost = 'idealocareer'
 const greenhouseHost = 'fulfillment'
 const smartrecruitersHost = 'smartrecruiters'
@@ -17,8 +38,13 @@ const smartrecruitersHost = 'smartrecruiters'
  * }) */
 
 smartrecruiters.getJobs({
-	hostname: smartrecruitersHost,
-	city: city
+ 	hostname: smartrecruitersHost,
+ 	city: city
 }).then(jobs => {
-	console.log('smartrecruiters jobs', jobs)
+ 	/* console.log('smartrecruiters jobs', jobs) */
+	index.saveObjects(jobs).then(({ objectsIds }) => {
+		console.log(objectsIds)
+	}).catch(err => {
+		console.log(err)
+	})
 })
--- providers/smartrecruiters.js
+++ providers/smartrecruiters.js
@@ -8,17 +8,27 @@ const fetch = require('node-fetch')
 	 - https://api.smartrecruiters.com/v1/companies/Mitie/postings
  */
 
+const providerId = 'smartrecruiters'
 const baseUrl = 'https://api.smartrecruiters.com/v1/companies'
 const jobPostingBaseUrl = `https://jobs.smartrecruiters.com`
 
+const serializeJobs = (jobs, hostname) => {
+	return jobs.map(job => {
+		return {
+			/* the one needed for algolia */
+			objectID: `${providerId}-${hostname}-${job.uuid}`,
+			title: job.name,
+			url: `${jobPostingBaseUrl}/${hostname}/${job.id}`,
+			publishedDate: job.releasedDate
+		}
+	})
+}
+
 const getJobs = async ({
 	hostname,
-	country,
-	city
+	country
 }) => {
-	/* wants city with fist letter upercase */
-	const cityName = city.charAt(0).toUpperCase() + city.slice(1)
-	const url = `${baseUrl}/${hostname}/postings?city=${cityName}`
+	const url = `${baseUrl}/${hostname}/postings`
 
 	let jobs = null
 	try {
@@ -31,7 +41,7 @@ const getJobs = async ({
 		console.log('error fetching jobs', error)
 	}
 
-	return jobs
+	return serializeJobs(jobs, hostname)
 }
 
 exports.getJobs = getJobs
