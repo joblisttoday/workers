6bcfd2faeb90f8a6a36f9bf5606c54230469c45f
hugurp <hugurp@pm.me>
2020-12-17 08:24:13 +0100

    algolia-jobs out of index

---
+++ algolia-jobs.js
@@ -0,0 +1,97 @@
+const config = require('dotenv').config()
+
+const algoliasearch = require("algoliasearch")
+
+const recruitee = require('./providers/recruitee')
+const greenhouse = require('./providers/greenhouse')
+const smartrecruiters = require('./providers/smartrecruiters')
+
+const database = require('./database')
+
+/*
+	 algolia config
+ */
+
+let algoliaAppId = null
+let algoliaApiKey = null
+let indexName = null
+
+if (process.env.NODE_ENV === 'production') {
+	indexName = 'prod_jobs'
+	algoliaAppId = process.env['ALGOLIA_PROD_APPLICATION_ID']
+	algoliaApiKey = process.env ['ALGOLIA_PROD_ADMIN_API_KEY']
+} else {
+	indexName = 'dev_jobs'
+	algoliaAppId = config.parsed['ALGOLIA_DEV_APPLICATION_ID']
+	algoliaApiKey = config.parsed['ALGOLIA_DEV_ADMIN_API_KEY']
+}
+
+if (!algoliaAppId || !algoliaApiKey || !indexName) {
+	console.log('Required algoliaAppId && algoliaApiKey && indexName')
+	return
+}
+
+const client = algoliasearch(algoliaAppId, algoliaApiKey)
+const index = client.initIndex(indexName)
+
+
+/*
+	 test the providers
+ */
+
+const providerMethods = {
+	recruitee: recruitee.getJobs,
+	greenhouse: greenhouse.getJobs,
+	smartrecruiters: smartrecruiters.getJobs
+}
+const city = 'berlin'
+
+const init = async () => {
+	let companies
+
+	try {
+		companies = await database.getCompanies()
+	} catch (error) {
+		console.log('Error getting companies from local folder', error)
+		return
+	}
+
+	const promises = companies.map(company => {
+		const providerMethod = providerMethods[company.provider]
+		if (typeof providerMethod === 'function') {
+			return providerMethod({
+				hostname: company.hostname,
+				companyTitle: company.title
+			})
+		} else {
+			return null
+		}
+	})
+
+	Promise.all(promises).then(responses => {
+		let allJobs = []
+		responses.filter(res => res).forEach(jobs => {
+			jobs.forEach(job => {
+				allJobs.push(job)
+			})
+		})
+
+		console.info('Jobs:', allJobs.length)
+
+		if (process.env.NODE_ENV === 'production') {
+			index.replaceAllObjects(allJobs).then(({ objectsIds }) => {
+ 				console.info('algolia save success')
+ 			}).catch(err => {
+ 				console.log('algolia save error', err)
+ 			})
+		} else {
+			console.info('Dev: algolia upload has been skipped')
+		}
+	}).catch(err => {
+		console.error(err)
+	})
+
+}
+
+/* launch the script */
+init()
