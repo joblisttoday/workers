293f1462f64bacd1bd1e0c3d4fd19f3066e8df84
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-08-17 11:55:36 +0200

    try fix sql generation of job.url

--- src/sqlite/jobs_trigger.sql
+++ src/sqlite/jobs_trigger.sql
@@ -1,11 +1,11 @@
 CREATE TRIGGER IF NOT EXISTS jobs_after_insert AFTER INSERT ON jobs BEGIN
 INSERT OR REPLACE INTO jobs_fts (rowid, objectID, name, location, company_slug, company_title)
-VALUES (new.rowid, new.objectID, new.name, new.location, new.company_slug, new.company_title);
+VALUES (new.rowid, new.objectID, new.url, new.name, new.location, new.company_slug, new.company_title);
 END;
 
 CREATE TRIGGER IF NOT EXISTS jobs_after_update AFTER UPDATE ON jobs BEGIN
 INSERT OR REPLACE INTO jobs_fts (rowid, objectID, name, location, company_slug, company_title)
-VALUES (new.rowid, new.objectID, new.name, new.location, new.company_slug, new.company_title);
+VALUES (new.rowid, new.objectID, new.url, new.name, new.location, new.company_slug, new.company_title);
 END;
 
 CREATE TRIGGER IF NOT EXISTS jobs_after_delete AFTER DELETE ON jobs BEGIN
