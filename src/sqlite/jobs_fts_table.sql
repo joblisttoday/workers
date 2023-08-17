b6e7173cb62c1894993415698298ebdc82ea453a
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-08-17 14:04:38 +0200

    also add published date

--- src/sqlite/jobs_fts_table.sql
+++ src/sqlite/jobs_fts_table.sql
@@ -1,3 +1,3 @@
 CREATE VIRTUAL TABLE IF NOT EXISTS jobs_fts USING fts5 (
-	objectID, name, url, location, company_slug, company_title
+	objectID, name, url, location, published_date, company_slug, company_title
 );
