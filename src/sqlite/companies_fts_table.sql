e291a9e0ee6243ef69f04c5c6cb449f377058759
h.u.g.u.rp <h.u.g.u.rp@pm.me>
2023-12-07 00:08:49 +0100

    try fts3 vs fts5

--- src/sqlite/companies_fts_table.sql
+++ src/sqlite/companies_fts_table.sql
@@ -1,3 +1,3 @@
-CREATE VIRTUAL TABLE IF NOT EXISTS companies_fts USING fts5 (
+CREATE VIRTUAL TABLE IF NOT EXISTS companies_fts USING fts3 (
 	slug, title, description, tags, positions
 );
--- src/sqlite/jobs_fts_table.sql
+++ src/sqlite/jobs_fts_table.sql
@@ -1,3 +1,3 @@
-CREATE VIRTUAL TABLE IF NOT EXISTS jobs_fts USING fts5 (
+CREATE VIRTUAL TABLE IF NOT EXISTS jobs_fts USING fts3 (
 	objectID, name, url, location, published_date, company_slug, company_title
 );
