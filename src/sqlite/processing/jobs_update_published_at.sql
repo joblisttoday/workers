5947eb0c0d9c92aee3607937d17e9fdf34c489a9
ug.rp <ugrp@duck.com>
2024-02-18 14:20:35 +0100

    update job.published_date when null (left empty)

--- src/sqlite/processing/jobs_update_published_at.sql
+++ src/sqlite/processing/jobs_update_published_at.sql
@@ -3,7 +3,6 @@ UPDATE jobs
 SET published_date = (
     CASE
         WHEN published_date LIKE '1%' THEN date(datetime(published_date, 'unixepoch'))
-        WHEN published_date IS NULL THEN '2012-01-01'
         ELSE date(published_date)
     END
 );
