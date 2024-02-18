9861dee4138efa4f7bd16cd3753c2f9bc8980c99
ug.rp <ugrp@duck.com>
2024-02-18 12:07:34 +0100

    serialize all dates/timestamps into YY-MM-DD

--- src/sqlite/companies_data_processing.sql
+++ src/sqlite/companies_data_processing.sql
@@ -3,9 +3,19 @@
 UPDATE companies
 SET created_at = (
     /* Condition to convert created_at from epoch to date when value starts by 1 */
-    CASE 
-        WHEN created_at LIKE '1%' THEN datetime(created_at, 'unixepoch')
-        WHEN created_at IS NULL THEN '2012-01-01 00:00:00'
-        ELSE created_at
+		CASE
+        WHEN created_at LIKE '1%' THEN date(datetime(created_at, 'unixepoch'))
+        WHEN created_at IS NULL THEN '2012-01-01'
+        ELSE date(created_at)
+    END
+);
+
+UPDATE companies
+SET updated_at = (
+    /* Condition to convert updated_at from epoch to date when value starts by 1 */
+		CASE
+        WHEN updated_at LIKE '1%' THEN date(datetime(updated_at, 'unixepoch'))
+        WHEN updated_at IS NULL THEN '2012-01-01'
+        ELSE date(updated_at)
     END
 );
--- src/sqlite/jobs_data_processing.sql
+++ src/sqlite/jobs_data_processing.sql
@@ -3,9 +3,9 @@
 UPDATE jobs
 SET published_date = (
     /* Condition to convert created_at from epoch to date when value starts by 1 */
-    CASE 
-        WHEN published_date LIKE '1%' THEN datetime(published_date, 'unixepoch')
-        WHEN published_date IS NULL THEN '2012-01-01 00:00:00'
-        ELSE published_date
+    CASE
+        WHEN published_date LIKE '1%' THEN date(datetime(published_date, 'unixepoch'))
+        WHEN published_date IS NULL THEN '2012-01-01'
+        ELSE date(published_date)
     END
 );
