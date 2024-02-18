23a6755289740460eeee079d1437ddeec794610f
ug.rp <ugrp@duck.com>
2024-02-18 14:21:10 +0100

    update company.updated_at when null (created_at)

--- src/sqlite/processing/companies_update_updated_at.js
+++ src/sqlite/processing/companies_update_updated_at.js
@@ -3,7 +3,7 @@ UPDATE companies
 SET updated_at = (
 		CASE
         WHEN updated_at LIKE '1%' THEN date(datetime(updated_at, 'unixepoch'))
-        WHEN updated_at IS NULL THEN '2012-01-01'
+        WHEN updated_at IS NULL THEN date(created_at)
         ELSE date(updated_at)
     END
 );
