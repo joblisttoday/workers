646bc9faeea51249b034058559e40ade8a6175fd
ug.rp <ugrp@duck.com>
2024-03-04 21:20:17 +0100

    remove hightight on fts… no needed if join in sql client

--- src/sqlite/companies_trigger.sql
+++ src/sqlite/companies_trigger.sql
@@ -1,11 +1,11 @@
 CREATE TRIGGER IF NOT EXISTS companies_after_insert AFTER INSERT ON companies BEGIN
-INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions, is_highlighted)
-VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions, new.is_highlighted);
+INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions)
+VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions);
 END;
 
 CREATE TRIGGER IF NOT EXISTS companies_after_update AFTER UPDATE ON companies BEGIN
-INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions, is_highlighted)
-VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions, new.is_highlighted);
+INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions)
+VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions);
 END;
 
 CREATE TRIGGER IF NOT EXISTS companies_after_delete AFTER DELETE ON companies BEGIN
