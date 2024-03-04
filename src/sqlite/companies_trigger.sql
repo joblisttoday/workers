8dd4027733f416eb6d7fbca0e7b5474163923105
ug.rp <ugrp@duck.com>
2024-03-04 21:13:19 +0100

    add is_highlighted to src/sqlite/companies_trigger.sql for fts

--- src/sqlite/companies_trigger.sql
+++ src/sqlite/companies_trigger.sql
@@ -1,11 +1,11 @@
 CREATE TRIGGER IF NOT EXISTS companies_after_insert AFTER INSERT ON companies BEGIN
-INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions)
-VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions);
+INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions, is_highlighted)
+VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions, new.is_highlighted);
 END;
 
 CREATE TRIGGER IF NOT EXISTS companies_after_update AFTER UPDATE ON companies BEGIN
-INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions)
-VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions);
+INSERT OR REPLACE INTO companies_fts (rowid, slug, title, description, tags, positions, is_highlighted)
+VALUES (new.rowid, new.slug, new.title, new.description, new.tags, new.positions, new.is_highlighted);
 END;
 
 CREATE TRIGGER IF NOT EXISTS companies_after_delete AFTER DELETE ON companies BEGIN
